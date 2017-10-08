016aa8fc1769774ad4c6340d4bccfc5c51ed039b
Aleksander Woźniak <aleksanderwozniak96@gmail.com>
2017-10-08 16:35:13 +0200

    Added ManagerActivity

--- app/src/main/java/com/olq/timemanager/MainActivity.kt
+++ app/src/main/java/com/olq/timemanager/MainActivity.kt
@@ -1,60 +1,34 @@
 package com.olq.timemanager
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
-
-    var startTime: Long = System.currentTimeMillis()
-    var currentTime: Long = 0
-    var timeSavedState: Long = 0
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myToggle.setOnClickListener { onBtnClick() }
+        myAddButton.setOnClickListener{ onAddBtnClick() }
     }
 
 
-
-    fun onBtnClick(){
-        timeSavedState = currentTime
-        startTime = System.currentTimeMillis()
-
-        val handler = Handler()
-        val runnable = object : Runnable{
-            override fun run(){
-                updateTimer(handler, this)
-            }
-        }
-
-        handler.post { runnable.run() }
-    }
-
-
-    fun updateTimer(handler: Handler, runnable: Runnable){
-        if(myToggle.isChecked){
-            countTime()
-            handler.postDelayed(runnable, 1000)
+    fun onAddBtnClick(){
+        if(myEditText.text.isEmpty()){
+            onEditTextEmpty()
+        }else{
+            launchManagerActivity(myEditText.text.toString())
         }
     }
 
+    fun onEditTextEmpty(){
+        toast("Task name can't be empty")
+    }
 
-    fun countTime(){
-        currentTime = System.currentTimeMillis() - startTime + timeSavedState
-        var seconds: Int = (currentTime / 1000).toInt()
-        val minutes: Int = seconds / 60
-        val hours: Int = minutes / 60
-
-        seconds = seconds % 60
-
-        Log.d("countTime", "h: $hours, min: $minutes, s: $seconds")
-        myTimerText.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    fun launchManagerActivity(text: String){
+        startActivity<ManagerActivity>("TASK_TEXT" to text)
     }
 }
